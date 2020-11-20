72c8e8835bf83091039da81e8ae9b273b48025ef
Jeremy Miller <jeremy.miller@clarivate>
2020-11-20 14:27:38 -0800

    move methods

---
+++ trees/tree_node.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    """
+    Generic tree node class for building tree structures
+    """
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.val = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
+
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.right_child or self.left_child)
+
+    def has_any_children(self):
+        return self.right_child or self.left_child
+
+    def has_both_children(self):
+        return self.right_child and self.left_child
+
+    def replace_node_data(self, key, val, lc, rc):
+        self.key = key
+        self.val = val
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
